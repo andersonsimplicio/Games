796c5cf7e9efbe2d065973c36607e7879ce09dba
andersonsimplicio <anderson25eu@gmail.com>
2023-06-22 16:17:38 -0300

    Nova nave

--- StartWars/classes/Nave.py
+++ StartWars/classes/Nave.py
@@ -3,8 +3,8 @@ from .Laser import Laser
 class Nave(pygame.sprite.Sprite):
     def __init__(self,groups) -> None:
         super().__init__(groups)
-        self.__image = pygame.image.load(os.path.join("assets","img","ship.png")).convert_alpha()
-        self.__image = pygame.transform.scale(self.__image,(35,30))
+        self.__image = pygame.image.load(os.path.join("assets","img","nave_02.png")).convert_alpha()
+        self.__image = pygame.transform.scale(self.__image,(45,40))
         self.image = self.__image
         self.__rect = self.image.get_rect(center=(1200/2, 650/2))
         self.rect = self.__rect
