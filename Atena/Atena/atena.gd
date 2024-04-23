373134ae190dfc0f95fedc8e5fdc77e82660db2d
andersonsimplicio <anderson25eu@gmail.com>
2024-04-22 22:09:41 -0300

    Criação de Animação ataque

--- Atena/Atena/atena.gd
+++ Atena/Atena/atena.gd
@@ -4,14 +4,22 @@ extends CharacterBody2D
 @onready var animacoes:AnimationPlayer = get_node("AnimationPlayer")
  
 func _physics_process(_delta:float)->void:
+	if ataque():
+		animacoes.play("ataque")
 	animate()
 
 func animate()->void:
+	print(velocity,Vector2.ZERO)
 	if velocity != Vector2.ZERO:
 		pass	
 	else:
 		animacoes.play("idle")
 	
+
+func ataque()->bool:
+	if Input.is_action_pressed("Ataque"):
+		return true
+	return false
 
 func get_direction()->Vector2:
 	return Vector2(
